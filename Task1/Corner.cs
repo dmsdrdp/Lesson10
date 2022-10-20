1fa5142a18d31a6664efc2825ce010b6bd8dca95
Dmitriy <Dmitriy@DESKTOP-8IVVVVB>
2022-10-20 23:54:27 +0300

    Добавьте файлы проекта.

---
+++ Task1/Corner.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Task1
+{
+    class Corner
+    {
+        public double gradus;
+        public double min;
+        public double sec;
+        public double decimalDegree;
+
+        public double Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    gradus = 0;
+                    Console.WriteLine("0");
+                }
+
+                else
+                {
+                    gradus = value;
+                }
+            }
+        }
+
+        public double Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    min = 0;
+                    Console.WriteLine("0");
+                }
+                else if (value > 60)
+                {
+                    min = 60;
+                    Console.WriteLine("60");
+                }
+                else
+                {
+                    min = value;
+                }
+            }
+
+        }
+
+        public double Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    sec = 0;
+                    Console.WriteLine("0");
+                }
+                else if (value > 60)
+                {
+                    sec = 60;
+                    Console.WriteLine("60");
+                }
+                else
+                {
+                    sec = value;
+                }
+            }
+
+        }
+        public double ToRadians()
+        {
+            decimalDegree += (sec / 3600) + (min / 60) + gradus;
+            return decimalDegree * Math.PI / 180;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Corner corner = new Corner();
+            try
+            {
+                Console.WriteLine("Введите данные");
+                corner.Gradus = Convert.ToDouble(Console.ReadLine());
+                corner.Min = Convert.ToDouble(Console.ReadLine());
+                corner.Sec = Convert.ToDouble(Console.ReadLine());
+
+                corner.decimalDegree = 0;
+
+                double radians = corner.ToRadians();
+                Console.WriteLine("{0} радиан", radians);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
